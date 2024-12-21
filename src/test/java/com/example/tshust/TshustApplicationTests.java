662d795aa8189dd9d8b60a71ca78e6ec5b1ae4c3
Thi1503 <thile15032003@gmail.com>
2024-12-22 02:04:32 +0700

    khoi dong du an

---
+++ src/test/java/com/example/tshust/TshustApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tshust;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TshustApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
